ac150d9d77b3a05fa6ca84f49840f88ef86666d3
Rehaan Aria <rharia@students.everettcc.edu>
2025-05-28 14:39:00 -0700

    Initial Commit Test

---
+++ main.py
@@ -0,0 +1,25 @@
+# Yearly Budget and Expense Tracker
+
+# STEP 1: Display Welcome Message
+# *******************************
+# - Greet the user to the program
+# - Explain how the program uses the 50/30/20 rule
+# - Explain how the program's menu works
+
+# STEP 2: Design program's menu
+# *****************************
+# 1) Enter monthly income (ability to choose particular month)
+# 2) View budget breakdown (ability to choose particular month)
+# 3) Log an expense
+# 4) Remove an expense
+# 5) View expense history
+# 6) Check remaining budget
+# 7) Generate spending report (charts, etc)
+# 8) Exit
+
+# Step 3: Begin implementation of each menu item
+
+# STEP 4: MENU ITEM 1 (Enter monthly income)
+# ******************************************
+# - Show the user the contents of their budget.csv file -
+# 
