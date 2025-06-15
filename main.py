a6a0949f9fcb069d1e29ef8611eda206f34b5e1d
Rehaan Aria <rharia@students.everettcc.edu>
2025-06-15 09:46:43 -0700

    Tidy up main.py

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 # Yearly Budget and Expense Tracker
 from tracker_functions import *
-# STEP 1: Display Welcome Message
-# *******************************
-# - Greet the user to the program
-# - Explain how the program uses the 50/30/20 rule
-# - Explain how the program's menu works
 
+# Prints out the welcome message for the user
 print_welcome_message()
 
-# STEP 2: Design program's menu
-# *****************************
-# 1) Enter monthly income (ability to choose particular month)
-# 2) View budget breakdown (ability to choose particular month)
-# 3) Log an expense
-# 4) Remove an expense
-# 5) View expense history
-# 6) Check remaining budget
-# 7) Generate spending report (charts, etc)
-# 8) Exit
-
+# Prints out an introductory message telling the user to use the menu
 print_menu_instructions()
 
 is_exit = False
 
+# Loop the main menu continuously until the user hits 8 to exit
 while is_exit != True:
 
+    # Print out the menu to the user
     print_menu_options()
 
     user_menu_choice = get_menu_choice()
@@ -48,48 +36,3 @@ while is_exit != True:
             is_exit = True
         case _:
             print("N/A")
-    
-    
-
-# Step 3: Begin implementation of each menu item
-
-# STEP 4: MENU ITEM 1 (Enter monthly income)
-# ******************************************
-# - Show the user the contents of their budget.csv file -
-# - Ask the user for which month they would like to update their income
-# - Message indicating successful update
-# - Calculate the budget categories for month
-# - Store the values of the categories in respective cells in budget.csv
-
-# STEP 5: MENU ITEM 2 (View Budget Breakdwon)
-# *******************************************
-# - Ask the user for the month which they would like to access their budget breakdown
-# or if they would like to view the breakdown for each month
-
-# STEP 6: Log an Expense
-# **********************
-# - Ask the user for which month they would like to log an expense to open the correct monthly expense csv file
-# - Ask the user for the day of the expense
-# - Ask the user for the details of the expense - (category, detail, amount)
-# - Find the appropriate sorted place in the file to store the expense based on date
-
-# STEP 7: Remove an expense
-# *************************
-# - Ask the user for the month of the expense they are trying to remove
-# - Print out the table/list of expenses for that month from the corresponding csv file, and give each row
-# a corresponding number
-# - Let the user choose a number for a row, and that entry will be removed from the table and csv
-
-# STEP 8: View expense history
-# ****************************
-# - Show the total expense amount for each month to the user
-# - Ask the user if they would like to view the particular expenses for a particular month
-# - If they do want to view particular month, ask for the month then print the expenses
-
-# STEP 9: Check remaining budget
-# ******************************
-# - Print the remaining amount of budget for each category for each month
-
-# STEP 10: Generate Spending Report
-# *********************************
-# TBC
